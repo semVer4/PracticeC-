5f73f0e4832c5f9e66bb5c689c920dda800f1461
DevNikita85 <nikrozhko125@gmail.com>
2022-03-31 12:08:22 +0300

    3zd

---
+++ 3zd/z3/BinaryN.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z3.Models
+{
+    public class BinaryN
+    {
+        long number = default(long);
+        public long Number => number;
+        public BinaryN(string input)
+        {
+            number = Convert.ToInt64(input,2);
+        }
+        public static BinaryN operator+(BinaryN current, BinaryN add)
+        {
+            var addResult = current.Number + add.Number;
+            return new BinaryN(Convert.ToString(addResult,2));
+        }
+
+        public static BinaryN operator -(BinaryN current, BinaryN add)
+        {
+            var addResult = current.Number - add.Number;
+            return new BinaryN(Convert.ToString(addResult, 2));
+        }
+
+        public static BinaryN operator *(BinaryN current, BinaryN add)
+        {
+            var addResult = current.Number * add.Number;
+            return new BinaryN(Convert.ToString(addResult, 2));
+        }
+
+        public static BinaryN operator /(BinaryN current, BinaryN add)
+        {
+            var addResult = current.Number / add.Number;
+            return new BinaryN(Convert.ToString(addResult, 2));
+        }
+
+        public override string ToString()
+        {
+            return Convert.ToString(number,2);
+        }
+    }
+}
--- 3zd/z3/Program.cs
+++ 3zd/z3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using z3.Models;
 
 namespace z3
 {
@@ -10,11 +11,21 @@ namespace z3
     {
         static void Main(string[] args)
         {
-            var s1 = Console.ReadLine();
-            var s2 = Console.ReadLine();
+            //string s1 = Console.ReadLine();
+            //string s2 = Console.ReadLine();
+            
+            //Binary bin = new Binary(s1, s2);
+            //bin.BinSum();
 
-            Binary bin = new Binary(s1, s2);
-            bin.BinSum();
+
+            var bNumber1 = new BinaryN("00001");
+            var bNumber = new BinaryN("100101001");
+            var number = bNumber + bNumber1;
+            Console.WriteLine(bNumber);
+            Console.WriteLine(bNumber1);
+            Console.WriteLine(number);
+            //var diff = bNumber - bNumber1;
+            Console.ReadLine();
         }
     }
 }
