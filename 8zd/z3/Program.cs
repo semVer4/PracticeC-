dea08aae333f74b36dc71c6e47a6b20cfe2b01fd
DevNikita85 <nikrozhko125@gmail.com>
2022-04-03 21:35:47 +0300

    8zd

---
+++ 8zd/z3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace z3
+{
+    internal class Program
+    {
+        public static void SearchNumber(string text)
+        {
+            string pattern = @"\b(\d+\W?)";
+            Regex regex = new Regex(pattern);
+
+            foreach (Match match in regex.Matches(text))
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SearchNumber(Console.ReadLine());
+        }
+    }
+}
