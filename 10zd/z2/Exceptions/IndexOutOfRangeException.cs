91c821af2a25b7b5bdcc9537eba11304fe039c32
DevNikita85 <nikrozhko125@gmail.com>
2022-04-06 21:11:34 +0300

    10zd

---
+++ 10zd/z2/Exceptions/IndexOutOfRangeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace z2.Exceptions
+{
+    internal class IndexOutOfRangeException : Exception
+    {
+        public string Comment { get; }
+        public IndexOutOfRangeException(string comment = "Error")
+        {
+            Comment = comment;
+        }
+
+        public IndexOutOfRangeException(string message, string comment = "Error") :base(message)
+        {
+        }
+    }
+}
