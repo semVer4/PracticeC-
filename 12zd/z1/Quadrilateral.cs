831589bf8c86bcc5292672097bbf8c3403b23271
DevNikita85 <nikrozhko125@gmail.com>
2022-04-08 11:13:07 +0300

    New zd

---
+++ 12zd/z1/Quadrilateral.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace z1
+{
+    internal partial class Program
+    {
+        class Quadrilateral
+        {
+            public string name { get; set; }
+
+            public Quadrilateral(string name)
+            {
+                this.name = name;
+            }
+
+            public virtual void GetInfo()
+            {
+                Console.WriteLine($"Название: {name}");
+            }
+        }
+    }
+}
